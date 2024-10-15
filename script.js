c4090465a2dfc53779a9dc63ee42cd5d24fb7a82
Alice Alves <alveslice150@gmail.com>
2024-10-14 22:21:23 -0400

    primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const form = document.getElementById('sum-form')
+
+form.addEventListener('submit', function(event){
+        event.preventDefault();
+        const num = parseFloat(document.getElementById('number1').value);
+        const num2 = parseFloat(document.getElementById('number2').value);
+        alert("Soma = " + (num + num2));
+
+});
+
+console.log(form);
